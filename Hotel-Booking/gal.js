a64b85eb1e56b2f037e670843e555b978c9b57e2
Prakhar-121 <prakhartheunique@gmail.com>
2024-12-06 21:25:55 +0530

    first commit

---
+++ Hotel-Booking/gal.js
@@ -0,0 +1,9 @@
+// JavaScript for lightbox functionality
+function openLightbox(imgSrc) {
+    document.getElementById("lightbox-img").src = imgSrc;
+    document.getElementById("lightbox").style.display = "block";
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
